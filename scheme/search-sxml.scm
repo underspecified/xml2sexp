e9b9ece62fae52def88e471740a9fd511e298038
underspecified <underspecified@gmail.com>
2011-11-02 10:26:25 +0900

    Added new tool: search-sxml

---
+++ scheme/search-sxml.scm
@@ -0,0 +1,26 @@
+#!/usr/local/bin/csi -ss
+
+; search-sxml.scm: search sxml document for matching patterns
+; Eric Nichols <eric@ecei.tohoku.ac.jp>
+
+(use ssax sxpath sxpath-lolevel data-structures)
+
+(define (search-sxml path port)
+  (let ((sxml (ssax:xml->sxml port '())))
+    (pp ((sxpath path) sxml) ) ) )
+
+(define (main args)
+  (let* (;(path "/root/document/sentences/sentence/tokens/token")
+	 ;(path "//token/lemma")
+	 (path '(// (tokens 
+		     (// (token ((equal? (@ (id "1")))))))))
+	 (search (lambda (port) (search-sxml path port))))
+    (cond ((= 0 (length args)) ; read from stdin when no command line args 
+	   (search (current-input-port)))
+	  (else (for-each
+		 (lambda (port) (call-with-input-file port search))
+		 args)))))
+
+(cond-expand
+ (compiling (main (command-line-arguments)))
+ (else #f))
