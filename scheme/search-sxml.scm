0bab4dbba908af196e8cb9c052bb7ca02cc2cec7
underspecified <underspecified@gmail.com>
2012-03-26 13:28:03 +0900

    added test XML i/o

--- scheme/search-sxml.scm
+++ scheme/search-sxml.scm
@@ -3,7 +3,7 @@
 ; search-sxml.scm: search sxml document for matching patterns
 ; Eric Nichols <eric@ecei.tohoku.ac.jp>
 
-(use ssax sxpath sxpath-lolevel data-structures)
+(use ssax sxpath sxpath-lolevel data-structures regex)
 
 (define (search-sxml path port)
   (let ((sxml (ssax:xml->sxml port '())))
@@ -12,8 +12,10 @@
 (define (main args)
   (let* (;(path "/root/document/sentences/sentence/tokens/token")
 	 ;(path "//token/lemma")
-	 (path '(// (tokens 
-		     (// (token ((equal? (@ (id "1")))))))))
+	 ;(path '(// (tokens (// (token ((equal? (@ (id "1")))))))))
+	 ;(path "//token[word='blazei']")
+	 ;(path '(// (token ((equal? (word "blazei"))))))
+	 (path '(// (word (((lambda () #t) "blazei")))))
 	 (search (lambda (port) (search-sxml path port))))
     (cond ((= 0 (length args)) ; read from stdin when no command line args 
 	   (search (current-input-port)))
